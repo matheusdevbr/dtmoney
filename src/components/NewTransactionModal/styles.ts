d91eda893bca60bdf571cd609dd3f79bacaf49ad
Matheus Santiago <matheussantiagopmw@gmail.com>
2021-09-27 03:20:21 -0300

    alterando o estilo do botão de acordo com o tipo

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -55,34 +55,39 @@ export const TransactionTypeContainer = styled.div`
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem;
 
-    button {
-        height: 4rem;
-        border: 1px solid #d7d7d7;
-        border-radius: 0.25rem;
+ 
+`;
 
-        background: transparent;
+interface RadioBoxProps {
+    isActive: boolean;
+}
 
-        display: flex;
-        align-items: center;
-        justify-content: center;
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
 
-        transition: border-color 0.2s;
+    background: ${(props) => props.isActive ? '#ccc' : 'transparent'};
 
-        &:hover {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    transition: border-color 0.2s;
+
+    &:hover {
             border-color: ${darken(0.1, '#d7d7d7')};
-        }
+    }
 
-        img {
+    img {
             width: 20px;
             height: 20px;
-        }
+    }
 
-        span {
+    span {
             display: inline-block;
             margin-left: 1rem;
             font-size: 1rem;
             color: var(--text-title);
-        }
     }
-
 `;
